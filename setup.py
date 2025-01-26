48cc82bf581914a721ed7b3025fec094b1846bec
Nina <ninahaladova@gmail.com>
2025-01-26 17:31:13 +0100

    Resolved merge conflicts

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
    name='carpepy',
    version='0.0.1',
    description='Module for visualising polarised genomes',
-   author='Nina Daley & Stuart JE Baird',
+   author='Nina Daley & Stuart J.E. Baird',
    author_email='ninahaladova@gmail.com',
    packages=['carpepy'],
    install_requires=['matplotlib', 'numpy', 'pandas'],
