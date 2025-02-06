212b88c9a44f342efe356aaaead9137b2b23e88d
Nina <ninahaladova@gmail.com>
2025-02-06 20:21:20 +0100

    Fix setup.py initialization

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
    name='carpepy',
-   version='0.0.2',
+   version='0.0.4',
    description='Module for visualising polarised genomes',
    author='Nina Daley & Stuart J.E. Baird',
    author_email='ninahaladova@gmail.com',
-   packages=find_packages(),
+   packages=['carpepy'],
    install_requires=['matplotlib', 'numpy', 'pandas', 'scipy'],
-   download_url='https://github.com/Studenecivb/carpepy/archive/refs/tags/v0.0.1.tar.gz',
+   download_url='https://github.com/Studenecivb/carpepy/archive/refs/tags/v0.0.4.tar.gz',
 )
