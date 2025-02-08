d2be5330df73654ba3d91d89ddbde4969b42dd59
Nina <ninahaladova@gmail.com>
2025-02-08 21:33:49 +0100

    Update the version to 0.0.5

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
    name='carpepy',
-   version='0.0.4',
+   version='0.0.5',
    description='Module for visualising polarised genomes',
    author='Nina Daley & Stuart J.E. Baird',
    author_email='ninahaladova@gmail.com',
    packages=['carpepy'],
    install_requires=['matplotlib', 'numpy', 'pandas', 'scipy'],
-   download_url='https://github.com/Studenecivb/carpepy/archive/refs/tags/v0.0.4.tar.gz',
+   download_url='https://github.com/Studenecivb/carpepy/archive/refs/tags/v0.0.5.tar.gz',
 )
